f40f86ffcc3b47fe1091a664fbc25492c8436e3f
Anca Toma <130585552+AniaToma@users.noreply.github.com>
2023-06-28 14:19:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// hamburger menu
+hamburger = document.querySelector('.hamburger');
+hamburger.onclick = function(){
+    navbar = document.querySelector('.nav-bar');
+navBar.classList.toggle('active');
+}
